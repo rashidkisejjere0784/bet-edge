c5374cf446da82dcf7d4529b51e0896ffda98edd
Byansi Anthony <byansianthony@gmail.com>
2023-04-28 00:48:41 +0300

    Ft: fix the sidebar
    Dashboard navigation
    
    Co-Authored-By: BindyaPhilip <108077951+BindyaPhilip@users.noreply.github.com>
    Co-Authored-By: Kisejjere Rashid  <88730649+rashidkisejjere0784@users.noreply.github.com>
    Co-Authored-By: Priscillasky <101385360+Priscillasky@users.noreply.github.com>
    Co-Authored-By: Trevour Ssemaganda <semagandatrevour@gmail.com>

--- resources/views/layouts/navbars/auth/sidenav.blade.php
+++ resources/views/layouts/navbars/auth/sidenav.blade.php
@@ -69,7 +69,6 @@
                 <a class="nav-link {{  str_contains(request()->url(), 'Withdraw') == true ? 'active' : '' }}" href="{{ route('page', ['page' => 'Withdraw']) }}">
                     <div
                         class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                        <!-- <i class="ni ni-credit-card text-success text-sm opacity-10"></i> -->
                         <i class="ni ni-money-coins text-success text-sm opacity-10"></i>
                     </div>
                     <span class="nav-link-text ms-1">Withdraw</span>
