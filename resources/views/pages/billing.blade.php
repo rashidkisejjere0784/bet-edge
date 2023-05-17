28db091935edb7b124d63c609c22953dad62d5ff
Priscillasky <priscillasky542@gmail.com>
2023-05-17 18:34:16 +0300

    final

--- resources/views/pages/billing.blade.php
+++ resources/views/pages/billing.blade.php
@@ -78,79 +78,7 @@
                     </div> --}}
                 </div>
             </div>
-            {{-- <div class="col-lg-4">
-                <div class="card h-100">
-                    <div class="card-header pb-0 p-3">
-                        <div class="row">
-                            <div class="col-6 d-flex align-items-center">
-                                <h6 class="mb-0">Invoices</h6>
-                            </div>
-                            <div class="col-6 text-end">
-                                <button class="btn btn-outline-primary btn-sm mb-0">View All</button>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="card-body p-3 pb-0">
-                        <ul class="list-group">
-                            <li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
-                                <div class="d-flex flex-column">
-                                    <h6 class="mb-1 text-dark font-weight-bold text-sm">September, 26, 2023</h6>
-                                    <span class="text-xs">#MS-415646</span>
-                                </div>
-                                <div class="d-flex align-items-center text-sm">
-                                    $180
-                                    <button class="btn btn-link text-dark text-sm mb-0 px-0 ms-4"><i
-                                            class="fas fa-file-pdf text-lg me-1"></i> PDF</button>
-                                </div>
-                            </li>
-                            <li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
-                                <div class="d-flex flex-column">
-                                    <h6 class="text-dark mb-1 font-weight-bold text-sm">February, 04, 2023</h6>
-                                    <span class="text-xs">#RV-126749</span>
-                                </div>
-                                <div class="d-flex align-items-center text-sm">
-                                    $250
-                                    <button class="btn btn-link text-dark text-sm mb-0 px-0 ms-4"><i
-                                            class="fas fa-file-pdf text-lg me-1"></i> PDF</button>
-                                </div>
-                            </li>
-                            <li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
-                                <div class="d-flex flex-column">
-                                    <h6 class="text-dark mb-1 font-weight-bold text-sm">April, 05, 2023</h6>
-                                    <span class="text-xs">#FB-212562</span>
-                                </div>
-                                <div class="d-flex align-items-center text-sm">
-                                    $560
-                                    <button class="btn btn-link text-dark text-sm mb-0 px-0 ms-4"><i
-                                            class="fas fa-file-pdf text-lg me-1"></i> PDF</button>
-                                </div>
-                            </li>
-                            <li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
-                                <div class="d-flex flex-column">
-                                    <h6 class="text-dark mb-1 font-weight-bold text-sm">June, 25, 2023</h6>
-                                    <span class="text-xs">#QW-103578</span>
-                                </div>
-                                <div class="d-flex align-items-center text-sm">
-                                    $120
-                                    <button class="btn btn-link text-dark text-sm mb-0 px-0 ms-4"><i
-                                            class="fas fa-file-pdf text-lg me-1"></i> PDF</button>
-                                </div>
-                            </li>
-                            <li class="list-group-item border-0 d-flex justify-content-between ps-0 border-radius-lg">
-                                <div class="d-flex flex-column">
-                                    <h6 class="text-dark mb-1 font-weight-bold text-sm">March, 01, 2023</h6>
-                                    <span class="text-xs">#AR-803481</span>
-                                </div>
-                                <div class="d-flex align-items-center text-sm">
-                                    $300
-                                    <button class="btn btn-link text-dark text-sm mb-0 px-0 ms-4"><i
-                                            class="fas fa-file-pdf text-lg me-1"></i> PDF</button>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div> --}}
+            
         </div>
         
         <div class="row">
@@ -203,12 +131,7 @@
                                             <input type="number" name="odd" class="d-none" value="{{$game['oddBTTS']}}">
                                             <input type="submit" value="BTTS" class="btn btn-secondary text-white">
                                         </form>
-                                        {{-- <a class="btn btn-link text-dark px-3 mb-0" href="javascript:;"><i
-                                                class="fas fa-pencil-alt text-dark me-2" aria-hidden="true"></i>Edit</a> --}}
-
-                        
-                                        {{-- <a class="btn btn-link text-dark px-3 mb-0" href="javascript:;"><i
-                                                    class="fas fa-pencil-alt text-dark me-2" aria-hidden="true"></i>Edit</a> --}}
+                                     
                             
                                     </div>
                                     <div>
